6b7fda17f2d55f7a42eef1362fe433f6561fc0c9
david lusztig <davidlusztig@outlook.com>
2020-09-23 16:05:19 +0200

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,26 +4,36 @@
 # Examples:
 #
 require "open-uri"
-# require "faker"
+require "faker"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+Lead.destroy_all
+User.destroy_all
 
 puts 'Creating leads...'
 
 puts "Start time is #{Time.now}"
 puts "--------------------------"
 
-User.destroy_all
 
 puts 'Creating a fake user...'
+puts "--------------------------"
 
-User.create!(
+admin = User.new(
     email: "admin@demo.com",
     password: "password",
     admin: true)
+admin.save!
 
+    5.times do
+      Lead.create(
+          name: Faker::Company.name,
+        )
+    end
+puts "5 leads created"
 
 puts "finished seeding"
+puts "--------------------------"
 
 puts "#{User.count} users created"
 puts "--------------------------"
