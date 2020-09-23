f39f2696fff762d48657b32357234aea7491096d
david lusztig <davidlusztig@outlook.com>
2020-09-23 15:44:45 +0200

    all updated for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -86,7 +86,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_062629) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,30 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+# require "faker"
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+puts 'Creating leads...'
+
+puts "Start time is #{Time.now}"
+puts "--------------------------"
+
+User.destroy_all
+
+puts 'Creating a fake user...'
+
+User.create!(
+    email: "admin@demo.com",
+    password: "password",
+    admin: true)
+
+
+puts "finished seeding"
+
+puts "#{User.count} users created"
+puts "--------------------------"
+puts "End time is #{Time.now}"
+puts "--------------------------"
+puts 'Finished!'
