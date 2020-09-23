3c8f6030c2b6e6bbc2172a7ee441a3cfdd19806f
david lusztig <davidlusztig@outlook.com>
2020-09-23 18:05:43 +0200

    login footers fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,9 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
+  
 end
